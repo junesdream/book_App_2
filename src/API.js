2234b07c9ba792c74d5ee70c62adfe73d8e55199
junesdream <norisori@gmail.com>
2023-02-01 01:56:51 +0100

    first commit

---
+++ src/API.js
@@ -0,0 +1,2 @@
+export const API_URL = "https://example-data.draftbit.com/books?_limit=70";
+export const BOOK_DETAILS_URL = "https://example-data.draftbit.com/books";
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "../App";
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+   <div className="navbar">
+       <div><h1>Daily Books App</h1></div>
+       
+       <div><Link to="/favorites"><h3>Your Favorites</h3> </Link></div>
+   </div>
+  )
+}
+
+export default Navbar
